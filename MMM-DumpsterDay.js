2317634d67f641b25b6859922a95a7c61fb4c01b
DeanoNoire <rykala.dan@gmail.com>
2020-09-18 23:14:09 +0200

    Major functionalilty, logging and reusability updates.

--- MMM-DumpsterDay.js
+++ MMM-DumpsterDay.js
@@ -5,18 +5,18 @@ Module.register("MMM-DumpsterDay",{
 		pulse: true, // Make the module pulsating
 
 		enableBasic: true,
-		basicStartDate: "29.10.2019",
+		basicStartDate: "21.07.2020", // use DD.MM.YYYY format
 		basicDateDiff_1: 14,
 		basicNotifyDayBefore: true,
 
 
 		enablePlastic: true,
-		plasticStartDate: "16.01.2019", // use 01 instead of 1
+		plasticStartDate: "29.07.2020", // use DD.MM.YYYY format
 		plasticDateDiff_1: 28,
 		plasticNotifyDayBefore: true,
 
 		enableLandscape: false,
-		landscapeStartDate: "29.10.2019",
+		landscapeStartDate: "29.10.2019", // use DD.MM.YYYY format
 		landscapeDateDiff_1: 28,
 		landscapeNotifyDayBefore: true,
 	},
@@ -56,20 +56,17 @@ Module.register("MMM-DumpsterDay",{
 
 		if (enableBasic == true){
 		basicTrashDayResult = this.isBasicTrashDay();
-		Log.info("basicTrashDayResult: "+basicTrashDayResult);
 		var basicDiv = document.createElement("div");
 		basicDiv.className = "basicDiv";
 		wrapper.appendChild(basicDiv);
 			if (basicTrashDayResult == 0){
 				basicDiv.classList.add("non_today");
-				Log.info("zmizikuju basic");
 			}
 		}
 
 
 		if (enablePlastic == true){
 		plasticTrashDayResult = this.isPlasticTrashDay();
-		Log.info("plasticTrashDayResult: "+plasticTrashDayResult);
 		var plasticDiv = document.createElement("div");
 		plasticDiv.className = "plasticDiv";
 		wrapper.appendChild(plasticDiv);
@@ -81,7 +78,6 @@ Module.register("MMM-DumpsterDay",{
 
 		if (enableLandscape == true){
 		landscapeTrashDayResult = this.isLandscapeTrashDay();
-		Log.info("landscapeTrashDayResult: "+landscapeTrashDayResult);
 		var landscapeDiv = document.createElement("div");
 		landscapeDiv.className = "landscapeDiv";
 		wrapper.appendChild(landscapeDiv);
@@ -100,34 +96,19 @@ Module.register("MMM-DumpsterDay",{
 
 	socketNotificationReceived: function() {},
 
-		isBasicTrashDay: function(){;
+
+
+		isBasicTrashDay: function(){
 			var startDate = this.config.basicStartDate;
 			var startDateYear = startDate.substring(6,10);
 			var startDateMonth = startDate.substring(3,5);
 			var startDateDay = startDate.substring(0,2);
 			var datediff_1 = this.config.basicDateDiff_1;
 			var notifyDayBefore = this.config.basicNotifyDayBefore;
-			if (notifyDayBefore == true){
-				var datediff_2 = datediff_1-1;
-			}
-			else var datediff_2 = datediff_1
-
-			var today = new Date();
-			var result;
-			const todays = new Date(today.getFullYear(),today.getMonth(),today.getDate());
 			const beginning = new Date(startDateYear,startDateMonth-1,startDateDay);
-			
-			var difference = parseInt((todays-beginning)/(24*3600*1000));
-
-			if(difference < 0) {
-				difference = difference + datediff_1
-			}
-
-			if (difference%datediff_1 == 0 || difference%datediff_2 == 0) {
-					result = 1
-				}
-				else {result = 0}
-		return result;
+			var result;
+			result = this.calculateDay(datediff_1,notifyDayBefore,beginning,"Basic")
+			return result;
 		},
 
 
@@ -138,27 +119,10 @@ Module.register("MMM-DumpsterDay",{
 			var startDateDay = startDate.substring(0,2);
 			var datediff_1 = this.config.plasticDateDiff_1;
 			var notifyDayBefore = this.config.plasticNotifyDayBefore;
-			if (notifyDayBefore == true){
-				var datediff_2 = datediff_1-1;
-			}
-			else var datediff_2 = datediff_1
-
-			var today = new Date();
-			var result;
-			const todays = new Date(today.getFullYear(),today.getMonth(),today.getDate());
 			const beginning = new Date(startDateYear,startDateMonth-1,startDateDay);
-			
-			var difference = parseInt((todays-beginning)/(24*3600*1000));
-
-			if(difference < 0) {
-				difference = difference + datediff_1
-			}
-
-			if (difference%datediff_1 == 0 || difference%datediff_2 == 0) {
-					result = 1
-				}
-				else {result = 0}
-		return result;
+			var result;
+			result = this.calculateDay(datediff_1,notifyDayBefore,beginning,"Plastic")
+			return result;
 		},
 
 		isLandscapeTrashDay: function(){;
@@ -168,27 +132,55 @@ Module.register("MMM-DumpsterDay",{
 			var startDateDay = startDate.substring(0,2);
 			var datediff_1 = this.config.landscapeDateDiff_1;
 			var notifyDayBefore = this.config.landscapeNotifyDayBefore;
-			if (notifyDayBefore == true){
-				var datediff_2 = datediff_1-1;
-			}
-			else var datediff_2 = datediff_1
+			const beginning = new Date(startDateYear,startDateMonth-1,startDateDay);
+			var result;
+			result = this.calculateDay(datediff_1,notifyDayBefore,beginning,"Landscape")
+			return result;
+		},
 
+
+		calculateDay: function(datediff_1,notifyDayBefore,beginning,type){
+			
+			// Notify day before
+			if (notifyDayBefore == true){
+				var datediff_2 = 1;
+				}
+				else var datediff_2 = 0
+				
+			// Date comparison
 			var today = new Date();
 			var result;
 			const todays = new Date(today.getFullYear(),today.getMonth(),today.getDate());
-			const beginning = new Date(startDateYear,startDateMonth-1,startDateDay);
 			
 			var difference = parseInt((todays-beginning)/(24*3600*1000));
-
+	
+			// Logging
+			Log.info("********"+type+"*******");
+			Log.info("Day difference: "+difference+" x Days until next: "+(((difference%datediff_1)-datediff_1)*-1)+" ");
+		
+			// Handling if is set future date as start date
 			if(difference < 0) {
 				difference = difference + datediff_1
 			}
-
-			if (difference%datediff_1 == 0 || difference%datediff_2 == 0) {
+			
+			// Trashday today 
+			if (difference%datediff_1 == 0) {
 					result = 1
+					Log.info("It's trashday !!");
 				}
-				else {result = 0}
-		return result;
+
+			// Trashday tomorrow - only if notify day before is enabled
+				else if (difference%(datediff_1+datediff_2) == 0) {
+						result = 1
+						Log.info("Tomorrow is trashday !!");
+				}
+			// Not a trashday
+					else {
+					result = 0
+					Log.info("Not a trashday");
+					}
+	
+				return result;
 		},
 
 
